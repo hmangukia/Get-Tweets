8cb3a29aaf7fb47eea3f0767d15b26f68f2b847d
Hetal <hmangukia9@gmail.com>
2020-03-25 03:45:07 +0530

    improved css

--- fetch/views.py
+++ fetch/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 import tweepy
+import json
 
 def home(request):
   if request.method == "POST":
@@ -7,10 +8,10 @@ def home(request):
     username = request.POST['username']
     hashtag = request.POST['hashtag']
     count = request.POST['count']
-    consumer_key = "your consumer key here"
-    consumer_secret = "your consumer secret key here"
-    access_token = "your access token here"
-    access_token_secret = "your access tokey secret key here"
+    consumer_key = "euF2vpAKFSeffdlvY6OLGKjyb"
+    consumer_secret = "PDukSba2b7QnQJQPwju8lQhMl0LcJmuK06C0hxs0kY9B7M77ER"
+    access_token = "3503980812-Tt3BTqPf2u7FtUeLRtwJvjTzdtqRZ47O6EhdQzU"
+    access_token_secret = "8lAmwKFB8Kg5xv6Rd8dQ7uVoHnpA5ZD60GL24hgTcdD0l"
     # Creating the authentication object
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
     # Setting your access token and secret
@@ -22,16 +23,20 @@ def home(request):
     if hashtag == "" and username != "":
       for tweet in api.user_timeline(id=username, count=count):
         tweets.append(tweet.text)
+        #print(json.dumps(user._json))
     elif username == "" and hashtag != "":
       hashtag = "#" + hashtag
       for tweet in api.search(q=hashtag, lang="en", count=count):
         hashtags.append(tweet.text)
+        #print(json.dumps(hashtag._json))
     elif username != "" and hashtag != "":
       hashtag = "#" + hashtag
       for tweet in api.user_timeline(id=username, count=count):
         tweets.append(tweet.text)
+        #print(json.dumps(user._json))
       for tweet in api.search(q=hashtag, lang="en", count=count):
         hashtags.append(tweet.text)
+        #print(json.dumps(hashtag._json))
     else:
       tweets.append("No Data")
       hashtags.append("No Data")
